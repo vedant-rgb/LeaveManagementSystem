c158b1dfae4f750b3db86f3db97e0d8ca23ad2fc
vedant-rgb <ghumarevedant25@gmail.com>
2025-03-21 15:38:31 +0530

    updated security

--- src/main/java/com/leaveManagement/PictLeaveProcessing/Entity/User.java
+++ src/main/java/com/leaveManagement/PictLeaveProcessing/Entity/User.java
@@ -11,8 +11,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
